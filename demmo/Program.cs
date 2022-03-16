a0346aaf82e45e13813dc19be063a0b9b2926c0f
Arati121 <avbhosale@mitaoe.ac.in>
2022-03-16 10:50:32 +0530

    Array function sort,reverse,clear copy

--- demmo/Program.cs
+++ demmo/Program.cs
@@ -6,8 +6,54 @@ namespace demmo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-           Console.WriteLine("hii");
+            // Console.WriteLine("Hello World!");
+            //Console.WriteLine("hii");
+
+            int[] arr = new int[6] { 105, 101, 100, 102, 99, 58 };
+            Console.WriteLine("_____printing array______________");
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("_______printing string array________________");
+            string[] days = new string[7] { "monday", "tuseday", "wensday", "thrustday", "friday", "saturday", "sunday" };
+            for(int i=0;i<days.Length;i++)
+            {
+              Console.WriteLine(days[i]);
+            }
+            Console.WriteLine("________sorting string array______________");
+            Array.Sort(days);
+            for(int i=0;i<days.Length;i++)
+            {
+                Console.WriteLine(days[i]);
+            }
+            Console.WriteLine("________sorting int array______________");
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("____________reverse int array______________");
+            Array.Reverse(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            //Array.Copy(days, 1, arr, 0, 2);
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+            Console.WriteLine("_______________clear array_________");
+            Array.Clear(arr, 2, 4);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+
+
         }
     }
 }
